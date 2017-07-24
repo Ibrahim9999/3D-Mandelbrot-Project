2e5ca484c5701c16096c20533fa32a653ed28917
ProfessorG <grant.vandomelen@gmail.com>
2017-07-24 10:39:26 -0700

    c files

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glut.h>
+
+#define EXIT_SUCCESS 0
+
+int main(int argc, char* argv[]) {
+
+    return EXIT_SUCCESS;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef {HEADER_NAME}
+#define {HEADER_NAME}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
