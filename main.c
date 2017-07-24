e6b8846184de9ed738f8fd2efe1ba4c191b5b544
vikk <vkashyap737@gmail.com>
2017-07-24 10:51:11 -0700

    Added test main loop

--- main.c
+++ main.c
@@ -5,7 +5,35 @@
 
 #define EXIT_SUCCESS 0
 
+//Render Funcion
+void render() {
+    //Clear
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+		glVertex3f(-1.0f,-1.0f, 0.0f);
+		glVertex3f( 1.0f, 0.0f, 0.0);
+		glVertex3f( 0.0f, 1.0f, 0.0);
+	glEnd();
+
+    glutSwapBuffers();
+}
+
+//Main
 int main(int argc, char* argv[]) {
+
+    //Setup window
+    glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(600,600);
+	glutCreateWindow("3D Mandelbulb Viewer");
+
+    //Setup loop
+    glutDisplayFunc(render);
+
+    //Loop
+    glutMainLoop();
 
     return EXIT_SUCCESS;
 }
