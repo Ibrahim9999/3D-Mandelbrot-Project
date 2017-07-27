5abc7ec6e025dd6cb78151ca99b0acd1e1493d5c
Ibrahim Mahmoud <Ibrahim Mahmoud>
2017-07-27 22:30:13 +0100

    Removed ins in vector.h

--- vector.h
+++ vector.h
@@ -31,23 +31,23 @@ typedef struct vector4f {
 float v3f_length(vec3f v);
 vec3f v3f_normalize(vec3f v);
 
-vec4f QuatFromDoubleVec(in double d, in vec3f v);
-vec4f QuatFromAxisAngle(in double angle, in vec3f axis);
-vec4f QuatQuatAdd(in vec4f a, in vec4f b);
-vec4f QuatQuatSubtract(in vec4f a, in vec4f b);
-vec4f QuatQuationMultiply(in vec4f a, in vec4f b);
-vec3f QuatVecMultiply(in vec4f q, in vec3f v);
-vec4f QuatQuatDivide(in vec4f a, in vec4f b);
-vec4f QuatDoubleDivide(in vec4f a, in double b);
-vec4f QuatInverse(in vec4f q);
-vec4f QuatExp(in vec4f a);
-vec4f QuatLn(in vec4f a);
-vec4f QuatPower(in vec4f q, in double p);
-void MoveForward(inout vec3f position, in vec3f forward, in double scalar);
-void ApplyRotationToVector(in vec4f rotation, inout vec3f axis);
-void InitializeCamera(inout vec3f facingForward, inout vec3f cameraPosition, inout vec3f depthAxis, inout vec3f horizontalAxis, inout vec3f verticalAxis);
-void Yaw(in double angle, inout vec3f vector in vec3f horizontalAxis, in vec3f verticalAxis, in vec3f depthAxis);
-void Pitch(in double angle, inout vec3f vector in vec3f horizontalAxis, in vec3f verticalAxis, in vec3f depthAxis);
-void Roll(in double angle, inout vec3f vector in vec3f horizontalAxis, in vec3f verticalAxis, in vec3f depthAxis);
+vec4f QuatFromDoubleVec(double d, vec3f v);
+vec4f QuatFromAxisAngle(double angle, vec3f axis);
+vec4f QuatQuatAdd(vec4f a, vec4f b);
+vec4f QuatQuatSubtract(vec4f a, vec4f b);
+vec4f QuatQuationMultiply(vec4f a, vec4f b);
+vec3f QuatVecMultiply(vec4f q, vec3f v);
+vec4f QuatQuatDivide(vec4f a, vec4f b);
+vec4f QuatDoubleDivide(vec4f a, double b);
+vec4f QuatInverse(vec4f q);
+vec4f QuatExp(vec4f a);
+vec4f QuatLn(vec4f a);
+vec4f QuatPower(vec4f q, double p);
+void MoveForward(out vec3f position, vec3f forward, double scalar);
+void ApplyRotationToVector(vec4f rotation, out vec3f axis);
+void InitializeCamera(out vec3f facingForward, out vec3f cameraPosition, out vec3f depthAxis, out vec3f horizontalAxis, out vec3f verticalAxis);
+void Yaw(double angle, out vec3f vector vec3f horizontalAxis, vec3f verticalAxis, vec3f depthAxis);
+void Pitch(double angle, out vec3f vector vec3f horizontalAxis, vec3f verticalAxis, vec3f depthAxis);
+void Roll(double angle, out vec3f vector vec3f horizontalAxis, vec3f verticalAxis, vec3f depthAxis);
 
 #endif
